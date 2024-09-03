ff6358a3c2b053bb6b90616208c0abbb9abcea1c
thingsym <thingsym@gmail.com>
2024-09-03 15:17:48 +0900

    fix: add path

--- command/db-backup.sh
+++ command/db-backup.sh
@@ -5,4 +5,4 @@ shopt -s expand_aliases
 
 alias docker-wp='docker run -it --rm --volumes-from $(docker compose --project-name `echo $(pwd) | awk -F "/" '"'"'{ print $NF }'"'"'` ps -q wordpress) --network container:$(docker compose --project-name `echo $(pwd) | awk -F "/" '"'"'{ print $NF }'"'"'` ps -q wordpress) wordpress:cli'
 
-docker-wp db export /var/www/backup/backup-`date +%Y%m%d%H%M%S`.sql
+docker-wp --path=/var/www/html db export /var/www/backup/backup-`date +%Y%m%d%H%M%S`.sql
